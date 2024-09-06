2875aa2970e4ff5badcf8e93b2e1cc8ae880383b
ChristianHopf <cehopf@gmail.com>
2024-09-06 13:42:29 -0400

    Init commit

---
+++ PartsApi/Models/Part.cs
@@ -0,0 +1,9 @@
+﻿namespace PartsApi.Models
+{
+    public class Part
+    {
+        public Guid Id { get; set; }
+        public string? partNumber { get; set; }
+        public string? name { get; set; }
+    }
+}
---
+++ PartsApi/Models/PartsContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PartsApi.Models
+{
+    public class PartsContext : DbContext
+    {
+        public PartsContext(DbContextOptions<PartsContext> options) : base(options) { }
+
+        public DbSet<Part> Parts { get; set; } = null!;
+    }
+}
